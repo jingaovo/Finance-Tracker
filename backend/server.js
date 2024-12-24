4f2547753c538d3164c041ae9cf4428d6e59caea
Jin <nancygoodluck233@gmail.com>
2024-12-24 11:04:30 -0500

    Database Connection Setup

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const homeRoutes = require('./routes/homeRoutes');
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+connectDB()
+
 app.use('/', homeRoutes);
 
 const PORT = 5001;
